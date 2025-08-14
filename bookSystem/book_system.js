9ca1513d334ad0aecc1ef001f77ef34898a16193
tienellia <sbriggs@telcoe.com>
2025-08-14 15:31:09 -0400

    add delete button

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -27,8 +27,9 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
-    );
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>
+        `);
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
 
@@ -47,4 +48,10 @@ function editbook(index) {
             document.getElementById('authorName').value = '';
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
+ }
+
+ function deletebook(index) {
+    books.splice(index, 1); // Remove old Entry
+    showbooks(); //Refresh list
+
  }
