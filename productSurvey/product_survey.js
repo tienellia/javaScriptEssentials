524a79780e21cfcf66b5047f75fd26a003363890
tienellia <sbriggs@telcoe.com>
2025-08-14 11:44:07 -0400

    user experience

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userexperience = document.getElementById('userExperienceText').value;
     const submitButton=document.getElementById('submitBtn');
     submitButton.onclick = submitFeedback;
     alert('Thank you for your valuable feedback')
@@ -16,6 +17,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userexperience;
     document.getElementById('userInfo').style.display = 'block';
 }
 
